2a3b89488e1e0f71e35d932ea50addd65feec526
Todd <twitzel@gmail.com>
2014-06-11 19:14:53 -0600

    updated to 0.2.9 -fixed error handling for socket errors

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 
-var pmp = require('pmp');
+var pmp = require('/pmp');
 // this should return your external ip address as reported by your gateway - not from an external site
 
 // the first parameter is your gateway if you know it - otherwise it will try to find it.
