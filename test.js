ed52dc2f8aa91e600e9d8cbe50b9206dfc8d7616
Todd <twitzel@gmail.com>
2015-02-10 14:39:25 -0700

    start of upnp integration

--- test.js
+++ test.js
@@ -1,16 +1,22 @@
 
-var pmp = require('/pmp');
+var pmp = require('./lib/pmp.js');
 // this should return your external ip address as reported by your gateway - not from an external site
 
 // the first parameter is your gateway if you know it - otherwise it will try to find it.
 
-pmp.getExternalAddress('',function(err,rslt){
+
+pmp.findGateway("",function(err,rslt){
     console.log(err,rslt);
 });
 
 
-
-pmp.portMap('',3000,3000,20,function(err,rslt){
-    console.log(err,rslt);
-});
+//pmp.getExternalAddress('10.6.1.1',function(err,rslt){
+//    console.log(err,rslt);
+//});
+//
+//
+//
+//pmp.portMap('',8888,8888,20,function(err,rslt){
+//    console.log(err,rslt);
+//});
 
